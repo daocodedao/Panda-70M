856e835ed2d98e70e2a2058dca3f44f7be7f4b15
linzhiji <linzhiji@gmail.com>
2024-03-21 10:51:18 +0800

    fix bug

--- captioning/video_llama/models/video_llama.py
+++ captioning/video_llama/models/video_llama.py
@@ -185,7 +185,8 @@ class VideoLLAMA(Blip2Base):
                 llama_model,
                 torch_dtype=torch.bfloat16, # torch.float16,
                 load_in_8bit=True,
-                device_map={'': device_8bit}
+                device_map={'': device_8bit},
+                device_map="auto"
             )
         else:
             self.llama_model = LlamaForCausalLM.from_pretrained(
