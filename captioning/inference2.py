669ac2af86023f6126d8ff3f477938b88935de6b
linzhiji <linzhiji@gmail.com>
2024-03-21 16:37:24 +0800

    fix bug

--- captioning/inference2.py
+++ captioning/inference2.py
@@ -33,6 +33,7 @@ cfg = Config(args)
 model_config = cfg.model_cfg
 model_config.device_8bit = args.gpu_id
 model_cls = registry.get_model_class(model_config.arch)
+print(f"model_cls:{model_cls}")
 model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
 print(f"model.eval()")
 model.eval()
